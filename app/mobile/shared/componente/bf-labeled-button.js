865eb1c5d04dd6e577520dd3831817237e8928d8
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-11-22 14:14:04 +0200

    Added new shared components

---
+++ app/mobile/shared/componente/bf-labeled-button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, StyleSheet, View, Image} from 'react-native';
+
+const BfLabeledButton = (props) => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+
+export default BfTextInput; 
