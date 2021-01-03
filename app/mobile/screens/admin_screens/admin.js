984bb4668a3680abf17ac745a38145bb74988e59
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2021-01-03 19:52:43 +0200

    Added auth logic + profile screen is gettting user data

--- app/mobile/screens/admin_screens/admin.js
+++ app/mobile/screens/admin_screens/admin.js
@@ -4,17 +4,38 @@ import {
     Text,
     StyleSheet
 } from 'react-native';
+import { CommonActions } from '@react-navigation/native';
 
+import BfLabeledButton from '../../shared/componente/bf-labeled-button';
+import * as storage from '../../shared/logic/storage-requester';
 
 export default class Admin extends React.Component {
     constructor(props) {
         super(props);
     }
-   
+
+    handleDisconectRequest = () => {
+        storage.removeItem('user')
+            .then(() => {
+                this.props.navigation.dispatch(
+                    CommonActions.reset({
+                        index: 0,
+                        routes: [
+                            { name: 'Acasa' },
+                        ],
+                    }));
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <View>
-                <Text>Admin</Text>
+                <Text>Pentru a folosi rolul de administrator, te rugam sa folosesti pagina web.</Text>
+                <BfLabeledButton
+                    title="Deconectare"
+                    onPress={() => this.handleDisconectRequest()}
+                />
             </View>
         )
     }
